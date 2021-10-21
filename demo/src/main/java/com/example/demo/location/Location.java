aea755f3eda55fea67f465bb71e020704642bc46
eminyilmazz <eminyilmzz@gmail.com>
2021-10-21 20:00:10 +0300

    First

---
+++ demo/src/main/java/com/example/demo/location/Location.java
@@ -0,0 +1,24 @@
+package com.example.demo.location;
+
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "locations")
+@EntityListeners(AuditingEntityListener.class)
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+}
---
+++ demo/src/main/java/com/example/demo/location/LocationController.java
@@ -0,0 +1,35 @@
+package com.example.demo.location;
+
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/")
+public class LocationController {
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @GetMapping("/locations")
+    public List<Location> getAllLocations() { return locationRepository.findAll(); }
+
+    @GetMapping("/locations/{id}")
+    public ResponseEntity<Location> getLocationById(@PathVariable(value = "id") Long locationId)
+        throws Exception {
+        Location location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new Exception ("Location " + locationId + " not found"));
+        return ResponseEntity.ok().body(location);
+    }
+    @GetMapping("/locations?{longitude}&{latitude}")
+    public ResponseEntity<Location> getLocationByCoordinates(@RequestParam(value = "longitude") double longitude, @RequestParam(value = "latitude") double latitude)
+        throws Exception {
+            Location location = locationRepository.findByCoordinates(longitude, latitude)
+                 .orElseThrow(() -> new Exception ("Longitude: " + longitude + "\t Latitude: " + latitude + " not found."));
+            return ResponseEntity.ok().body(location);
+    }
+    @PostMapping("/locations")
+    public Location createLocation(@RequestBody Location location) { return locationRepository.save(location);}
+}
